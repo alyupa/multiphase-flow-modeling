7aaa8c196c5bd221701aa04e7c792ccc35d37b68
Anastasia Lyupa <anastasiya.lyupa@gmail.com>
2013-11-17 15:40:16 +0400

    [IMPROVE] sh scripts
    and remove everything about shmem

---
+++ sh/build.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# ADD: remove temp files
+# ADD: test script parameters:
+#	* processors_count time_limit_in_minutes !=0
+#	* if "no", then processors_count = 1
+# ADD: text color
+
+# Usage description
+if [ -z "$2" ]; then
+	echo Use: $0 architecture communication [energy] [debug]
+	echo architecture: cpu,gpu
+	echo communication: no,mpi
+	exit
+fi
+
+exe_name="$1_$2"
+
+# Energy mode
+if [ "$3" = "energy" ]; then
+	debug="-D ENERGY"
+	exe_name="$exe_name_energy"
+	energy="../energy.cpp"
+	echo "Energy is enabled"
+else
+	energy=""
+	echo "Energy is not enabled"
+fi
+
+# Debug mode
+if [ "$3" = "debug" or "$4" = "debug" ]; then
+	debug="-D MY_TEST"
+	exe_name="$exe_name_debug"
+fi
+
+hostname=`hostname`
+
+if [ "$hostname" = "mvse" ]; then
+	ARCH=13
+	PPN="-ppn 1"
+	lib_path="-L/common/cuda/lib -lcudart"
+else 	if [ "$hostname" = "k100" ]; then
+		ARCH=20
+		PPN="-ppn 3"
+		lib_path="-L/common/cuda/lib64 -lcudart"
+	else
+		ARCH=20
+		PPN="-ppn 1"
+		lib_path="-L/usr/lib/x86_64-linux-gnu/ -lcudart"
+	fi
+fi
+
+task_name="-D THREE_PHASE"
+project_file="three-phase.cpp"
+project_folder="Three-phase"
+
+if [ "$1" = "gpu" ]; then
+	echo "nvcc $task_name $debug -c -arch sm_$ARCH gpu.o ../gpu.cu"
+		  nvcc $task_name $debug -c -arch sm_$ARCH gpu.o ../gpu.cu
+	arch_file="gpu.o"
+else if [ "$1" = "cpu" ]; then
+		arch_file="../cpu.cpp ../$project_folder/$project_file"
+		lib_path=""
+		PPN=""
+	 else
+		echo "Error in architecture: $1 is not supported"
+		exit
+	 fi
+fi
+
+if [ "$2" = "no" ]; then
+	comm_file="../no_communication.cpp"
+else if [ "$2" = "mpi" ]; then
+		comm_file="../mpi.cpp"
+	else
+		echo "Error in communication: $3 is not supported"
+		exit
+	fi
+fi
+
+if [ "$hostname" = "k100" ]; then
+	  compilator="mpicxx"
+else 	if [ "$hostname" = "mvse" ]; then
+		compilator="mpicc"
+	else	if [ "$2" = "mpi" ]; then
+			compilator="mpicxx"
+		else
+			compilator="g++-4.4"
+		fi
+	fi
+fi
+
+mkdir ../$project_folder/Debug
+exe_name="$exe_name.px"
+
+echo "$compilator $task_name $debug $lib_path ../main.cpp $comm_file $energy ../shared_test.cpp ../gauss.cpp $arch_file -o ../$project_folder/Debug/$exe_name"
+	  $compilator $task_name $debug $lib_path ../main.cpp $comm_file $energy ../shared_test.cpp ../gauss.cpp $arch_file -o ../$project_folder/Debug/$exe_name
+
+exit
--- sh/run.sh
+++ sh/run.sh
@@ -1,172 +1,56 @@
 #!/bin/bash
-# ADD: remove temp files
-# ADD: test script parameters: 
-#	* processors_count time_limit_in_minutes !=0
-#	* if "no", then processors_count = 1
-# ADD: text color
 
-# Описание параметров запуска скрипта
-if [ -z "$5" ]; then
-    echo Use: $0 task_name architecture communication processors_count time_limit_in_minutes [debug/energy]
-    echo task_name: 2ph,3ph,bl
-    echo architecture: cpu,gpu
-    echo communication: no,mpi,shmem
-    exit
-fi
-
-# Режим отладки
-if [ "$6" = "debug" ]
-then
-    debug="-D MY_TEST"
-    debug_name="_$6"
-fi
-
-# Режим учета теплообмена
-if [ "$6" = "energy" ]
-then
-    debug="-D ENERGY"
-    debug_name="_$6"
-	energy="../energy.cpp"
-else
-	energy=""
-fi
-
-case $4 in
-   *[^0-9]* )
-	echo "Error in processors_count: $4 is not supported by CAPAZ" 
-	exit      
-         ;;
-    * )  
-         ;;
-esac
-
-case $5 in
-   *[^0-9]* )
-	echo "Error in time_limit_in_minutes: $5 is not supported by CAPAZ" 
-	exit      
-         ;;
-    * )  
-         ;;
-esac
-
-hostname=`hostname`
-#echo $hostname
-
-if [ "$hostname" = "mvse" ]
-then
-    ARCH=13
-    PPN="-ppn 1"
-	lib_path="-L/common/cuda/lib -lcudart"
-    maxtime="-maxtime $5"
-else 	if [ "$hostname" = "k100" ]
-	then
-	    ARCH=20
-	    PPN="-ppn 3"
-	    lib_path="-L/common/cuda/lib64 -lcudart"
-	    maxtime="-maxtime $5"
-	else
-	    ARCH=20
-	    PPN="-ppn 1"
-	    lib_path="-L/usr/local/cuda/lib64 -lcudart"
-	fi
-fi
-
-if [ "$1" = "2ph" ]
-then
-    task_name="-D TWO_PHASE"
-    project_file="two-phase.cpp"
-    project_folder="Two-phase"
-else
-	if [ "$1" = "3ph" ]
-	then
-	    task_name="-D THREE_PHASE"
-	    project_file="three-phase.cpp"
-	    project_folder="Three-phase"
-	else
-		if [ "$1" = "bl" ]
-		then
-		    task_name="-D B_L"
-		    project_file="b-l.cpp"
-		    project_folder="Backley-Leverett"
-		else
-			echo "Error in task_name: $1 is not supported by CAPAZ"
-			exit
-		fi
-	fi
-fi
-
-if [ "$2" = "gpu" ]
-then
-    echo "nvcc $task_name $debug -c -arch sm_$ARCH gpu.o ../gpu.cu"
-          nvcc $task_name $debug -c -arch sm_$ARCH gpu.o ../gpu.cu
-    arch_file="gpu.o"
-else if [ "$2" = "cpu" ]
-     then
-    	arch_file="../cpu.cpp ../$project_folder/$project_file"
-	    lib_path=""
-		PPN=""
-     else
-	echo "Error in architecture: $2 is not supported by CAPAZ"
+# Usage description
+if [ -z "$4" ]; then
+	echo Usage: $0 processors_per_node_count processors_count time_limit_in_minutes exe_name
 	exit
-     fi
 fi
 
-if [ "$3" = "no" ]
-then
-    comm_file="../no_communication.cpp"
-else if [ "$3" = "mpi" ]
-     then
-	comm_file="../mpi.cpp"
-     else if [ "$3" = "shmem" ]
-	  then
-	    comm_file="../shmem.cpp"
-	  else
-	    echo "Error in communication: $3 is not supported by CAPAZ"
-            exit
-	  fi
-     fi
-fi
+case $1 in
+   *[^0-9]* )
+	echo "Error in processors_per_node_count: $1 is not supported"
+	exit
+		 ;;
+	* )
+		 ;;
+esac
 
-if [ "$hostname" = "k100" ]
-then
-   if [ "$3" = "shmem" ]
-   then
-      compilator="shmemcc"
-   else
-      compilator="mpicxx"
-   fi
-else 	if [ "$hostname" = "mvse" ]
-	then		
-	    if [ "$3" = "shmem" ]
-   	    then
-      		compilator="shmemcc"
-		else
-			compilator="mpicc"
-	    fi
-	else
-	    if [ "$3" = "mpi" ]
-            then
-		compilator="mpicxx"
-	    else
-		compilator="g++-4.4"
-  	    fi
-	fi
-fi
+case $2 in
+   *[^0-9]* )
+	echo "Error in processors_count: $2 is not supported"
+	exit
+		 ;;
+	* )
+		 ;;
+esac
 
-if [ "$6" = "energy" ]
-then
-	echo "Energy is supported" 
+case $3 in
+   *[^0-9]* )
+	echo "Error in time_limit_in_minutes: $3 is not supported"
+	exit
+		 ;;
+	* )
+		 ;;
+esac
+
+if [ $1 = 0 ]; then
+	PPN=""
 else
-	echo "Energy is not supported" 
+	PPN="-ppn $1"
 fi
 
-mkdir ../$project_folder/Debug
 
-echo "$compilator $task_name $debug $lib_path ../main.cpp $comm_file $energy ../shared_test.cpp ../gauss.cpp $arch_file -o ../$project_folder/Debug/$2_$3$debug_name.px"
-      $compilator $task_name $debug $lib_path ../main.cpp $comm_file $energy ../shared_test.cpp ../gauss.cpp $arch_file -o ../$project_folder/Debug/$2_$3$debug_name.px
+if [ $3 = 0 ]; then
+	maxtime=""
+else
+	maxtime="-maxtime $3"
+fi
+
+exe_name=$4
+project_folder="Three-phase"
 
 cd ../$project_folder/Debug
-echo "mpirun $PPN -np $4 $maxtime ../$project_folder/Debug/$2_$3$debug_name.px"
-mpirun $PPN -np $4 $maxtime ./$2_$3$debug_name.px
+echo "mpirun $PPN -np $2 $maxtime ../$project_folder/Debug/$exe_name"
+mpirun $PPN -np $2 $maxtime ./$exe_name
 
 exit
